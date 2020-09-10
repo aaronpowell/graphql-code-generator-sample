e7bd5581cf63670727b8fc8be5e345b6f7f035ba
Aaron Powell <me@aaron-powell.com>
2020-09-10 12:12:45 +1000

    starting point

--- api/graphql/resolvers.ts
+++ api/graphql/resolvers.ts
@@ -1,62 +1,7 @@
-import { Resolvers } from "./generated";
-
-const resolvers: Resolvers = {
+const resolvers = {
   Query: {
-    question(_, { id }, { dataStore }) {
-      return dataStore.getQuestionById(id);
-    },
-    async getRandomQuestion(_, __, { dataStore }) {
-      const questions = await dataStore.getQuestions();
-      return questions[Math.floor(Math.random() * questions.length) + 1];
-    },
-  },
-  Question: {
-    answers(question) {
-      return question.incorrect_answers
-        .concat([question.correct_answer])
-        .sort();
-    },
-    correctAnswer(question) {
-      return question.correct_answer;
-    },
-    id(question) {
-      return question.id;
-    },
-    question(question) {
-      return question.question;
-    },
-  },
-
-  Mutation: {
-    async answerQuestion(_, { id, answer }, { dataStore }) {
-      const question = await dataStore.getQuestionById(id);
-
-      return {
-        questionId: id,
-        question: question.question,
-        correctAnswer: question.correct_answer,
-        submittedAnswer: answer,
-        correct: question.correct_answer === answer,
-      };
-    },
-  },
-
-  Answer: {
-    correct({ correct }) {
-      return correct;
-    },
-    correctAnswer({ correctAnswer }) {
-      return correctAnswer;
-    },
-    question({ question }) {
-      return question;
-    },
-    questionId({ questionId }) {
-      return questionId;
-    },
-    submittedAnswer({ submittedAnswer }) {
-      return submittedAnswer;
-    },
+    question() {},
+    getRandomQuestion() {},
   },
 };
 
